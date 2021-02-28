171c4a4002a7aa2ab4b6bbcc9235b0676d1c8ee6
Yanis Dib <ynsprive@gmail.com>
2021-02-28 18:50:35 +0100

    Update Router
    Add PrivateRoute

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,25 +1,30 @@
-import { Route, Switch, BrowserRouter } from "react-router-dom";
-import { createBrowserHistory as createHistory } from "history";
-import App from "../App";
-import Header from "../components/Header/Header";
-import CreateAccount from "../components/Pages/CreateAccount/CreateAccount";
-import CreateUserProfile from "../components/Pages/CreateUserProfile/CreateUserProfile";
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory as createHistory } from 'history';
+
+import App from '../App';
+import PrivateRoute from './PrivateRoute';
+import Header from '../components/Header/Header';
+import CreateAccount from '../components/Pages/CreateAccount/CreateAccount';
+import CreateUserProfile from '../components/Pages/CreateUserProfile/CreateUserProfile';
+import Login from '../components/Pages/Login/Login';
+import Profile from '../components/Pages/Profile/Profile';
 
 export const history = createHistory();
 
 function AppRouter() {
-    return (
-        <BrowserRouter history={history}>
-            <Header />
-            <main>
-                <Switch>
-                    <Route path="/" component={App} exact={true} />
-                    <Route path="/sign-up" component={CreateAccount} />
-                    <Route path="/create-profile" component={CreateUserProfile} />
-                </Switch>
-            </main>
-        </BrowserRouter>
-    );
-};
+  return (
+    <BrowserRouter history={history}>
+      <Header />
+      <main>
+        <Switch>
+          <Route path='/' component={App} exact={true} />
+          <Route path='/sign-up' component={CreateAccount} />
+          <PrivateRoute path='/create-profile' component={CreateUserProfile} />
+          <Route path='/login' component={Login} />
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
+}
 
 export default AppRouter;
