2e23c33153e6bd5f5a158a264d11478cdfb88c09
Yanis Dib <ynsprive@gmail.com>
2021-03-09 18:49:06 +0100

    Update index.js
    + Conditional rendering

--- src/index.js
+++ src/index.js
@@ -6,19 +6,52 @@ import reportWebVitals from './reportWebVitals';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
+import { login } from './actions/auth';
+import { startGetCurrentUser } from './actions/user';
 import './assets/style/style.scss';
 
 const store = configureStore();
-store.dispatch(startGetCountries()).then(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <AppRouter />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-});
+const uid = !!localStorage.getItem('user') ? localStorage.getItem('user') : sessionStorage.getItem('user');
+let hasRendered = false;
+const renderApp = async () => {
+  await store.dispatch(startGetCountries());
+  if (!hasRendered) {
+    ReactDOM.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <AppRouter />
+        </Provider>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+    hasRendered = true;
+  };
+};
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
+
+const launchApp = async () => {
+  if (!!uid) {
+    await store.dispatch(login(uid));
+    renderApp();
+  }
+  else {
+    renderApp();
+  };
+};
+
+launchApp();
+
+// store.dispatch(startGetCountries()).then(() => {
+//  ReactDOM.render(
+//    <React.StrictMode>
+//      <Provider store={store}>
+//        <AppRouter />
+//      </Provider>
+//    </React.StrictMode>,
+//    document.getElementById('root')
+//  );
+//});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
