cbf9e809bf17fd3ecc8d8af86689161316364b2b
Yanis Dib <ynsprive@gmail.com>
2021-03-11 19:58:06 +0100

    Update index.js
    Dispatch of action 'startGetCountries' was moved to CreateAccount component.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { startGetCountries } from './actions/countries';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
 import { login } from './actions/auth';
-import { startGetCurrentUser } from './actions/user';
 import './assets/style/style.scss';
 
 const store = configureStore();
 const uid = !!localStorage.getItem('user') ? localStorage.getItem('user') : sessionStorage.getItem('user');
-let hasRendered = false;
-const renderApp = async () => {
-  await store.dispatch(startGetCountries());
-  if (!hasRendered) {
-    ReactDOM.render(
-      <React.StrictMode>
-        <Provider store={store}>
-          <AppRouter />
-        </Provider>
-      </React.StrictMode>,
-      document.getElementById('root')
-    );
-    hasRendered = true;
-  };
+const renderApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 };
-
-ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
 
 const launchApp = async () => {
   if (!!uid) {
@@ -41,17 +32,6 @@ const launchApp = async () => {
 };
 
 launchApp();
-
-// store.dispatch(startGetCountries()).then(() => {
-//  ReactDOM.render(
-//    <React.StrictMode>
-//      <Provider store={store}>
-//        <AppRouter />
-//      </Provider>
-//    </React.StrictMode>,
-//    document.getElementById('root')
-//  );
-//});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
